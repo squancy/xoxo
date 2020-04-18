e71da2ed36a51ecf3c63e90d40745b7db212caa3
Mark Frankli <mark@pearscom.com>
2020-04-18 15:47:20 +0200

    lite three version, play again

--- src/index.js
+++ src/index.js
@@ -77,49 +77,149 @@ function RecUI(props) {
   return <p>You are connected to {props.id}</p>;
 }
 
+function ConnUI() {
+  return <p>Connecting...</p>;
+}
+
 function createSquareVars(self) {
-  const history = self.state.history.slice(0, self.state.stepNumber + 1);
+	const history = self.state.history.slice(0, self.state.stepNumber + 1);
   const current = history[history.length - 1];
   const squares = current.squares.slice();
-  return [history, squares];
+	return [history, squares];
 }
 
 // Set states for both peers
 function setStateAfterClick(self, draw, i, turn, history, squares) {
-  self.setState({
-      history: history.concat([
-        {
-          squares: squares
-        }
-      ]),
-      stepNumber: history.length,
-      xIsNext: !self.state.xIsNext,
-      lastMoveIndex: self.state.lastMoveIndex.concat(i),
-      isDraw: draw,
-      isUserTurn: turn
-    });
-}
-
-/* 
-  Fires immediately when page is visited; listens for data send to user
-  Renders the changes made by the destination peer
-*/
-function receivePeerOuter(conn, peerObj, self) {
-  conn.on("data", function(data) {
-    if (data[0] === "a") {
-      return;
-    }
-    const index = Number(data.split("||")[0]);
-    const draw = data.split("||")[2] === "false" ? false : true;
-     const [history, squares] = createSquareVars(self);
-  
-    squares[index] = self.state.xIsNext ? "X" : "O";
-    setStateAfterClick(self, draw, index, true, history, squares);      
+	self.setState({
+    history: history.concat([
+      {
+        squares: squares
+      }
+    ]),
+    stepNumber: history.length,
+    xIsNext: !self.state.xIsNext,
+    lastMoveIndex: self.state.lastMoveIndex.concat(i),
+    isDraw: draw,
+    isUserTurn: turn
   });
 }
 
+function highlightWinner(winner) {
+  for (let sq of winner[0]) {
+    _('sqr_' + sq).style.color = "red";
+  }
+}
+
+function resetSquares() {
+  for (let i = 0; i < 9; i++) {
+    _("sqr_" + i).style.color = 'white';
+  }
+}
+
+/* 
+	Fires immediately when page is visited; listens for data send to user
+	Renders the changes made by the destination peer
+*/
+function receivePeerOuter(conn, peerObj, self) {
+  conn.on("data", function(data) {
+    if (data.score) {
+      scoreboardRef.updateScore(data.score);
+    }else if ((data.player2Again && self.state.localAgain) || 
+      (data.player1Again && self.state.localAgain)) {
+      resetStates(self);
+      self.state.connState.send({ receivedAgain: true });
+    } else if (data.receivedAgain) {
+      resetStates(self);
+    } else {
+      if (data.i === "a") {
+        return;
+      }
+      const index = data.i;
+      const squareToRemove = data.squareToRemove;
+      const draw = data.isDraw;
+      const [history, squares] = createSquareVars(self);
+    
+      squares[index] = self.state.xIsNext ? "X" : "O";
+      squares[squareToRemove] = null;
+      setStateAfterClick(self, draw, index, true, history, squares);	    
+    }
+  });
+}
+
+// Reset the states to their initial values when 'play again'
+function resetStates(self) {
+  self.setState({
+    history: [
+      {
+        squares: Array(9).fill(null)
+      }
+    ],
+    xIsNext: !self.state.isNext,
+    isDraw: false,
+    stepNumber: 0,
+    lastMoveIndex: [0],
+    isUserTurn: !self.state.isUserTurn,
+    movesShow: null,
+    player1Again: false,
+    player2Again: false,
+    localAgain: false
+  });  
+  self.trackSigns = [];
+  _('playAgain').innerHTML = 'Play Again';
+  _('playAgain').disabled = false;
+  if (_("show") !== null) _("show").setAttribute("id", "hide");
+  resetSquares();
+}
+
+var globalSquares;
+var globalConnObj;
+var scoreboardRef;
+
+// Keep track of the scores of X and O
+class Scoreboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShown: false,
+      XScore: 0,
+      YScore: 0,
+    }
+
+    this.updateScore = this.updateScore.bind(this);
+    scoreboardRef = this;
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      if (!globalSquares) return;
+      let isWin = calculateWinner(globalSquares);
+      if (isWin) {
+        this.updateScore(isWin[1]);
+        globalSquares = null;
+        globalConnObj.send({score: isWin[1]});
+      }
+    }, 1000);
+  }
+
+  updateScore(type) {
+    if (type === 'X') {
+      this.setState({ XScore: this.state.XScore + 1 });
+    } else {
+      this.setState({ YScore: this.state.YScore + 1 });
+    }
+  }
+
+  render() {
+    return (
+      <div className="scoreboard">
+        <span id="XWin">{this.state.XScore}</span> - <span id="YWin">{this.state.YScore}</span>
+      </div>
+    );
+  }
+}
+
 /*
-  Main component; renders whole UI & utilizes the components declared above
+	Main component; renders whole UI & utilizes the components declared above
 */
 class Game extends React.Component {
   constructor(props) {
@@ -145,10 +245,28 @@ class Game extends React.Component {
       connState: null,
       peerError: false,
       movesShow: null,
-      receiverUI: null
+      receiverUI: null,
+      player1Again: false,
+      player2Again: false,
+      localAgain: false,
+      scoreRecorded: false
     };
+    this.trackSigns = [];
     this.handleConnection = this.handleConnection.bind(this);
     this.setConnId = this.setConnId.bind(this);
+    this.playAgain = this.playAgain.bind(this);
+  }
+
+  // Play a new game without loosing the Peer JS id
+  playAgain(e) {
+    _('playAgain').innerHTML = 'Waiting for other player'; 
+    _('playAgain').disabled = true;
+    this.setState({ localAgain: true });
+    if (this.state.isUserTurn) {
+      this.state.connState.send({ player1Again: true });
+    } else {
+      this.state.connState.send({ player2Again: true });
+    }
   }
 
   // Fires when user clicks on a field
@@ -159,8 +277,11 @@ class Game extends React.Component {
     }
 
     const [history, squares] = createSquareVars(this);
-    const filledSquares = squares.filter(sq => sq).length;
+		const filledSquares = squares.filter(sq => sq).length;
     const connObj = this.state.connState;
+
+    globalConnObj = connObj;
+    globalSquares = squares;
 
     // Perform a lookahead to properly decide if match is draw
     let sqrBefore = squares[i];
@@ -168,13 +289,19 @@ class Game extends React.Component {
     let lookAhead = calculateWinner(squares);
     squares[i] = sqrBefore;
 
-    // Match is draw
+    // Check when match is draw; NOT USED IN THE LITE THREE VERSION
     if (filledSquares + 1 === squares.length && !lookAhead) {
       this.setState({ isDraw: true }, () => {
         // Callback send is needed because state is not updated asynchronously
         squares[i] = this.state.xIsNext ? "X" : "O";
-        connObj.send(i + "||" + squares[i] + "||" + this.state.isDraw); 
-        setStateAfterClick(this, this.state.isDraw, i, !this.state.isUserTurn, history, squares); 
+        connObj.send({
+          i,
+          square: squares[i],
+          isDraw: this.state.isDraw,
+          squareToRemove: -1
+        });
+        setStateAfterClick(this, this.state.isDraw, i, !this.state.isUserTurn, history,
+          squares); 
       });
       return;
       // Match is won / clicked on a filled field / not user's turn
@@ -186,9 +313,26 @@ class Game extends React.Component {
       return;
     }
 
-    // Update states to render UI; send data to destination peer to display user's move
+    /*
+      Implement a lite three version: when O or X puts the 4th sign remove the O or X that
+      the user has put in earliest
+    */
+    this.trackSigns.push(i);
+    let squareToRemove;
+    if (this.trackSigns.length > 3) {
+      _('sqr_' + this.trackSigns[0]).innerHTML = '';
+      squares[this.trackSigns[0]] = null;
+      squareToRemove = this.trackSigns[0];
+      this.trackSigns.shift();
+    }
+
     squares[i] = this.state.xIsNext ? "X" : "O";
-    connObj.send(i + "||" + squares[i] + "||" + this.state.isDraw); 
+    connObj.send({
+      i,
+      square: squares[i],
+      isDraw: this.state.isDraw,
+      squareToRemove
+    });
     setStateAfterClick(this, this.state.isDraw, i, !this.state.isUserTurn, history, squares); 
   }
 
@@ -200,10 +344,18 @@ class Game extends React.Component {
         _("btn_" + i).style.color = "white";
       }
     }
+
     this.setState({
       stepNumber: step,
       xIsNext: step % 2 === 0
     });
+
+    // Make sure winner trio is only marked as red at the last step
+    resetSquares();
+    if (step === this.state.history.length) {
+      let winner = calculateWinner(this.state.history[step]);
+      highlightWinner(winner);
+    }
   }
 
   // Load Peer JS library after UI has loaded;
@@ -262,11 +414,11 @@ class Game extends React.Component {
       return;
     }
     /*
-      peerObj: window.Peer ...
-      connId: destination peer ID
-      uid: my ID
-      conn: peer.connect(connId); ...
-    */
+			peerObj: window.Peer ...
+			connId: destination peer ID
+			uid: my ID
+			conn: peer.connect(connId); ...
+		*/
     let connId = this.state.connId;
     let peer = this.state.peerObj;
     let conn = this.state.connState;
@@ -280,6 +432,7 @@ class Game extends React.Component {
       connectPeer(conn, connId, this);
     });
     _("idInput").value = "";
+    this.setState({ receiverUI: ConnUI() });
   }
 
   setConnId(e) {
@@ -304,7 +457,7 @@ class Game extends React.Component {
           ? this.state.lastMoveIndex[move] % 3
           : 0;
       const desc = move
-        ? "Go to move #" + (move) + " (" + row + "; " + col + ")"
+        ? "Go to move #" + (move) + " (" + (row + 1) + "; " + (col + 1) + ")"
         : "Go to game start";
 
       return (
@@ -323,15 +476,18 @@ class Game extends React.Component {
       status = "Draw";
       if (_("hide") !== null) _("hide").setAttribute("id", "show");
     } else if (winner) {
-      for (let sq of winner[0]) {
-        _('sqr_' + sq).style.color = "red";
-      }
+      highlightWinner(winner);
       status = "Winner: " + winner[1];
       if (_("hide") !== null) _("hide").setAttribute("id", "show");
     } else {
-      let nextUserId = this.state.isUserTurn
-        ? this.state.uid
-        : this.state.connId;
+      let nextUserId = this.state.uid;
+      if (!nextUserId) {
+        nextUserId = 'Loading...';
+      } else {
+        nextUserId = this.state.isUserTurn
+          ? this.state.uid
+          : this.state.connId;
+      }
       status =
         "Next player: " +
         (this.state.xIsNext ? "X" : "O") +
@@ -343,6 +499,7 @@ class Game extends React.Component {
     return (
       <React.Fragment>
         <div className="verticalCont">
+          <Scoreboard />
           <div className="game">
             <div className="game-board">
               <Board
@@ -350,11 +507,10 @@ class Game extends React.Component {
                 onClick={i => this.handleClick(i)}
               />
             </div>
-            <div className="game-info"></div>
           </div>
           <br />
-          <div>{status}</div>
-          <p>My id is <b>{this.state.uid}</b></p>
+          <div>{this.state.connState ? status : ''}</div>
+          <p>My id is <b>{!this.state.uid ? 'Loading...' : this.state.uid}</b></p>
           <input
             type="text"
             placeholder="Enter destination peer ID"
@@ -378,7 +534,17 @@ class Game extends React.Component {
           {closedConn ? <p>Oops... your peer closed the connection.</p> : ""}
           {discConn ? <p>Oops... your peer has disconnected.</p> : ""}
           {browserSupport ? <p>Current browser is not supported.</p> : ""}
-          <ol id="hide">{moves}</ol>
+          <ol id="hide">
+            <button
+              key="playAgainButton"
+              className="w3-button w3-round-xxlarge w3-border w3-border-white w3-border-white"
+              id="playAgain"
+              onClick={this.playAgain}
+              >
+              Play again
+            </button>
+            {moves}
+          </ol>
         </div>  
       </React.Fragment>
     );
